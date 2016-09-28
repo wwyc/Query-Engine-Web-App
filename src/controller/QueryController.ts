868a66c0306dba82606e7445d446d9bab77f9943
aliciatang07 <tangxuejie96@163.com>
2016-09-28 14:42:33 -0700

    new version

--- src/controller/QueryController.ts
+++ src/controller/QueryController.ts
@@ -2,8 +2,12 @@
  * Created by rtholmes on 2016-06-19.
  */
 
-import {Datasets} from "./DatasetController";
+import {Datasets, default as DatasetController} from "./DatasetController";
 import Log from "../Util";
+import {bodyParser} from "restify";
+
+
+
 
 export interface QueryRequest {
     GET: string|string[];
@@ -18,8 +22,11 @@ export interface QueryResponse {
 export default class QueryController {
     private datasets: Datasets = null;
 
+
+
     constructor(datasets: Datasets) {
         this.datasets = datasets;
+
     }
 
     public isValid(query: QueryRequest): boolean {
@@ -30,9 +37,232 @@ export default class QueryController {
     }
 
     public query(query: QueryRequest): QueryResponse {
-        Log.trace('QueryController::query( ' + JSON.stringify(query) + ' )');
+        Log.trace('QueryController::query( ' + JSON.stringify(query) + ' )');//json string
 
         // TODO: implement this
-        return {status: 'received', ts: new Date().getTime()};
+
+        /*   1. parse the query(json format)
+         2. transfer the key in datasets to what I want
+         3. get ,where, order(chose a quick sort method), as ,
+         4.check if the dataset in memory or disk
+         5. return as which kind of data structure, */
+
+
+//parse the json querey to string
+
+        let get: any = query.GET;//string or array of string
+        let where:any= query.WHERE;
+        //json object or json array
+        let order: string = query.ORDER;
+        let format: string = query.AS;
+        let intermediate:Array<any>=this.dealWithWhere(where,get);
+        let result:Array<any>;
+        if(order!=null)
+        { let values: Array<number>;
+            for( var i=0;i<intermediate.length;i++)
+            {
+                for( var j=0;j<intermediate[i].length;j++)
+                    if(intermediate[i][j].key===order)
+                        values+=intermediate[i][j].value;
+            }
+            if (typeof values[0]==='number')
+                intermediate=this.quickSortNumber(values,intermediate,0,values.length-1);
+            if (typeof values[0]==='string')
+                intermediate=this.quickSortLetter(values,intermediate,0,values.length-1);
+        }
+        result= this.represent(get,intermediate);
+        result=JSON.parse('"render:"'+format+'","+"result"+":"'+result);
+        //JSON.parse
+        //return back to JSON object
+        return {result:result};
+       // return {status: 'received', ts: new Date().getTime()};
     }
+
+
+
+
+
+//deal with where
+    private  dealWithWhere(where:any,get:string):Array<any> {
+
+        let arr:Array<any>;//dictionary
+        let file:any=this.datasets[this.stringPrefix(get)];
+
+        for (var i=0;i<file.length;i++)
+        { for (var j=0;j<file[i].length;j++)
+        { if (this.parserEBNF(where,file[i][j]))
+        { arr+=file[i][j];
+        } }}
+
+        return arr;
+    }
+
+
+    private stringPrefix(get:string):string{
+        let prefix:string;
+        prefix=prefix.split("_")[0];
+        return prefix;
+    }
+
+    private parserEBNF(where:any,dataset:any):Boolean {
+        //GT= > EQ= LT<
+        //AND OR NOT
+        // parse where
+        //implemenntion of EBNF
+        // and or follow array，
+        // for loop
+        let valid:Boolean=true;
+
+        if (where.key === "AND"||"OR")
+        {           for(var i=0;i<where.key.value.length-1;i++)
+        {
+            if (where.key === "AND")
+            {valid= valid&&(this.parserEBNF(where.key[i],dataset)
+                &&this.parserEBNF( where.key[i+1],dataset));}
+            if (where.key==="OR")
+            {valid=valid&& (this.parserEBNF(where.key[i],dataset)
+                ||this.parserEBNF( where.key[i+1],dataset));}
+        }
+
+        }
+
+        if (where.key==="GT" || "EQ" || "LT") {
+            var key1:any=where.key.value.key;
+            if (where.key === "GT") {
+                valid = valid&&(dataset.key1.value > key1.value);
+            }
+
+            if (where.key === "EQ") {
+                valid = valid&&(dataset.key1.value === key1.value);
+            }
+
+            if (where.key === "LT") {
+                valid =valid&&(dataset.key1.value < key1.value);
+            }
+        }
+
+        if (where.key==="IS")
+
+        {valid= valid&&(dataset.key1.value
+            === where.key.value.key.value);}
+        if(where.key==="NOT") {
+            for (var j = 0; j < where.key.length; j++) {
+                valid =valid&&(!this.parserEBNF(where.key.value, dataset));
+            }
+        }
+        return valid;
+    }
+
+
+
+//list in order in matter of chose data structure: mergesort?quicksort?
+//    sort according to key’s value
+
+
+    private   quickSortNumber(arr1: Array<any>,arr2:Array<any>,left: number,right: number):Array<any> {
+        let pivot:number;
+        if(left< right)
+        {
+            pivot=this.partitionNumber(arr1,arr2,left,right);
+            this.quickSortNumber(arr1,arr2,left,pivot-1);
+            this.quickSortNumber(arr1,arr2,pivot+1,right);
+        }
+        return arr2;
+    }
+
+    private partitionNumber(arr1: Array<any>,arr2:Array<any>, left: number, right: number):number{
+        let middle:number=left + (right - left) / 2;
+        let pivot = arr1[middle];
+        this.swap(arr1[middle],arr1[left]);
+        this.swap(arr2[middle],arr2[left]);
+        let a:number = left + 1;
+        let b:number = right;
+        while (a<=b) {
+            while(a <= b && arr1[a] <= pivot) {
+                a++;
+            }
+
+            while(a <= b && arr1[b] > pivot) {
+                b--;
+            }
+
+            if (a < b) {
+                this.swap(arr1[a], arr1[b]);
+                this.swap(arr2[a], arr2[b]);
+            }
+        }
+        this.swap(arr1[a - 1],arr1[left]);
+        this.swap(arr2[a - 1],arr2[left]);
+        return a - 1;
+    }
+
+    private swap(left:any,right:any){
+        let temp:any=left;
+        left=right;
+        right=temp;
+    }
+
+    //sort words alphabetically
+    private quickSortLetter(arr1:Array<any>,arr2:Array<any>, left:number,right:number):Array<any>{
+        let pivot:number;
+
+        if(left< right)
+        {
+            pivot=this.partitionString(arr1,arr2,left,right);
+            this.quickSortLetter(arr1,arr2,left,pivot-1);
+            this.quickSortLetter(arr1,arr2,pivot+1,right);
+        }
+        return arr2;
+    }
+
+    private partitionString(arr1: Array<any>,arr2:Array<any>, left: number, right: number):number{
+        let middle:number=left + (right - left) / 2;
+        let pivot = arr1[middle];
+        this.swap(arr1[middle],arr1[left]);
+        this.swap(arr2[middle],arr2[left]);
+        let a:number = left + 1;
+        let b:number = right;
+        while (a<=b) {
+            while(a <= b && arr1[a].charCodeAt(0)<=pivot) //a<right 需不需要,
+            {
+                a++;
+            }
+
+            while(a <= b && arr1[b].charCodeAt(0)>pivot) {
+                b--;
+            }
+
+            if (a < b) {
+                this.swap(arr1[a], arr1[b]);
+                this.swap(arr2[a], arr2[b]);
+            }
+        }
+        this.swap(arr1[a - 1],arr1[left]);
+        this.swap(arr2[a - 1],arr2[left]);
+        return a - 1;
+    }
+
+    private represent(arr1:any, arr2:Array<any>):Array<any>{
+
+        let arr3: Array<any>;
+        if (typeof arr1!=='string')
+        { for ( var i=0;i<arr2.length-1;i++) {
+            arr3[i]= "{";
+            for (var j = 0; j < arr1.length-1; j++) {
+                arr3[i] =arr3[i] +arr1[j] + ":" + arr2[i].arr1[j].value+",";
+            }
+            arr3[i]=arr3[i]+arr1[arr1.length-1] + ":" + arr2[i].arr1[arr1.length-1].value+"}" }
+
+            arr3[i] += "{"+arr1[arr1.length - 1] + ":" + arr2[i].arr1[arr1.length - 1].value+"}";
+        }
+        else
+        { for ( var a=0;a<arr2.length-1;a++) {
+            arr3[i] = "{"+arr1[j] + ":" + arr2[i].arr1[j].value + "}"+",";
+        }
+            arr3[i] += "{"+arr1[arr1.length - 1] + ":" + arr2[i].arr1[arr1.length - 1].value+"}";
+        }
+        return arr3;
+
+    }
+
 }
--- test/QueryControllerSpec.ts
+++ test/QueryControllerSpec.ts
@@ -35,14 +35,15 @@ describe("QueryController", function () {
         expect(isValid).to.equal(false);
     });
 
-    it("Should be able to query, although the answer will be empty", function () {
-        // NOTE: this is not actually a valid query for D1, nor is the result correct.
-        let query: QueryRequest = {GET: 'food', WHERE: {IS: 'apple'}, ORDER: 'food', AS: 'table'};
-        let dataset: Datasets = {};
-        let controller = new QueryController(dataset);
-        let ret = controller.query(query);
-        Log.test('In: ' + JSON.stringify(query) + ', out: ' + JSON.stringify(ret));
-        expect(ret).not.to.be.equal(null);
-        // should check that the value is meaningful
-    });
-});
+    /*it("Should be able to query, although the answer will be empty", function () {
+     // NOTE: this is not actually a valid query for D1, nor is the result correct.
+     let query: QueryRequest = {GET: 'food', WHERE: {IS: 'apple'}, ORDER: 'food', AS: 'table'};
+     let dataset: Datasets = {};
+     let controller = new QueryController(dataset);
+     let ret = controller.query(query);
+     Log.test('In: ' + JSON.stringify(query) + ', out: ' + JSON.stringify(ret));
+     expect(ret).not.to.be.equal(null);
+     // should check that the value is meaningful
+     });*/
+     });
+
