4da85809d67058310aa2c450ad9788034e7808af
aliciatang07 <tangxuejie96@163.com>
2016-09-29 09:45:27 -0700

    fix bugs

--- src/controller/QueryController.ts
+++ src/controller/QueryController.ts
@@ -256,6 +256,7 @@ export default class QueryController {
     public represent(arr1:any, arr2:Array<any>):Array<any>{
 
         let arr3: Array<any>;
+
         if (typeof arr1!=='string')
         { for ( var i=0;i<arr2.length-1;i++) {
             arr3[i]= "{";
