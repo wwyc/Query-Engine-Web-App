388b030ac91052541d79120b2d74511bc0d5d5fb
aliciatang07 <tangxuejie96@163.com>
2016-10-09 10:26:49 -0700

    fix bugs

--- src/controller/QueryController.ts
+++ src/controller/QueryController.ts
@@ -4,7 +4,8 @@
 
 import {Datasets, default as DatasetController} from "./DatasetController";
 import Log from "../Util";
-
+import {bodyParser} from "restify";
+import {stringify} from "querystring";
 
 export interface QueryRequest {
     GET: string|string[];
@@ -24,53 +25,101 @@ export default class QueryController {
     }
 
     public isValid(query: QueryRequest): boolean {
-        if (typeof query !== 'undefined' && query !== null && Object.keys(query).length > 0) {
-            return true;
+
+        var isValidResult = false
+
+//Log.trace(query.WHERE.toString())
+//
+//Log.trace(typeofquery.WHERE)
+//Log.trace(Object.keys(query.WHERE).length.toString())
+//
+//Log.trace(Object.keys(query.WHERE).toString())
+//
+//Log.trace(typeofquery)
+//
+//Log.trace((typeofquery=='undefined').toString())
+
+        if ((typeof query == 'undefined')
+            || (query == null)
+            || (Object.keys(query).length < 2)
+            || (query.AS !== "TABLE")
+
+        ) {
+            return false;
         }
-        return false;
+
+//checkifWHEREexistsorisitempty
+        if ((typeof query.WHERE == 'undefined') || query.WHERE == null) {
+            return false
+        } else if (Object.keys(query.WHERE).length < 1) {
+            return false
+        }
+
+
+        if (typeof query.GET === 'string') {
+//checkifGETkeyisvalid&checkifORDERisequalinGETkey
+            if (this.isvalidKey(query.GET) && (query.ORDER == null || query.ORDER == query.GET)) {
+                isValidResult = true
+            }
+
+        } else if (Array.isArray(query.GET)) {
+
+//gothrougheachelementofarrayandcheckifGETkeyisvalid
+            for (var j = 0; j < Object.keys(query.GET).length; j++) {
+                if (!this.isvalidKey(query.GET[j])) {
+                    return false
+                }
+            }
+
+//trytofindGETkeyinORDER
+            if (query.ORDER !== null) {
+                isValidResult = false
+                for (var j = 0; j < Object.keys(query.GET).length; j++) {
+                    if (query.ORDER == null || query.GET[j] == query.ORDER) {
+                        isValidResult = true
+                    }
+                }
+
+            }
+
+
+        } else {
+            isValidResult = false
+        }
+
+
+        return isValidResult;
     }
 
     public query(query: QueryRequest): QueryResponse {
-        Log.trace('QueryController::query( ' + JSON.stringify(query) + ' )');
+        Log.trace('QueryController::query( ' + JSON.stringify(query) + ' )');//json string
 
         // TODO: implement this
-
-
-        var get = query.GET;
-        var where = query.WHERE;
+        //parse the json query to string
+        var get = query.GET;                            // can be string or array of string
+        var where = query.WHERE;                        //json object or json array
         var order = query.ORDER;
         var format = query.AS;
+
         var intermediate: any = [];
-        var orderValid: boolean = order !== null && typeof order !== 'undefined';
-        if (format !== "TABLE")
-            throw Error;
+
         if (typeof get === 'string') {
-            if (this.isvalidKey(get) === false)
-                throw Error;
-            if (get !== order && orderValid)
-                throw Error;
-            else
             intermediate = this.dealWithWhere(where, get)
         } else {
-            for (var i of get) {
-                if (this.isvalidKey(i) === false)
-                    throw Error;
-            }
-            if (!get.includes(order) && orderValid)
-                throw Error;
-            else
             intermediate = this.dealWithWhere(where, get[0])
         }
+
         var values: any = [];
+
         var finalResultObjArray: any = this.represent(get, intermediate);
+
         //Do this if order was requested
-        if (orderValid) {
-            if (this.isvalidKey(order) === false)
-                throw Error;
-            else
+        if (order !== null) {
             finalResultObjArray = this.sortArray(finalResultObjArray, order);
         }
-        Log.trace("this is FINAL result:  " + JSON.stringify(finalResultObjArray));
+
+        Log.trace("this is FINAL result:  " + JSON.stringify(finalResultObjArray))
+
         return {render: format, result: finalResultObjArray};
     }
 
@@ -82,7 +131,7 @@ export default class QueryController {
         var datasetsNew = JSON.parse(JSON.stringify(this.datasets))
 
         // Retrieve dataset from given GET
-        var datasetRetrived = datasetsNew[this.stringPrefix(get)];
+        var datasetRetrived = datasetsNew["courses"];
 
         var sections: any = []
 
@@ -109,10 +158,21 @@ export default class QueryController {
         return prefix;
     }
 
-
     public parserEBNF(where: any, section: any) {
 
         let valid = true;
+
+        if ((typeof where['AND'] == 'undefined')
+            && (typeof where['OR'] == 'undefined')
+            && (typeof where['GT'] == 'undefined')
+            && (typeof where['LT'] == 'undefined')
+            && (typeof where['EQ'] == 'undefined')
+            && (typeof where['IS'] == 'undefined')
+            && (typeof where['NOT'] == 'undefined')) {
+            throw Error
+        }
+        ;
+
         if (typeof where['AND'] !== 'undefined' || typeof where['OR'] !== 'undefined') {
             //  Log.trace("type1!!!")
             if (typeof where['AND'] !== 'undefined') {
@@ -120,8 +180,9 @@ export default class QueryController {
                 for (var ANDfilter of where['AND']) {
                     validList1.push(this.parserEBNF(ANDfilter, section));
                 }
-                //   Log.trace("validList1" + validList1);
+                //  Log.trace("validList1" + validList1);
 
+                //  Log.trace("validlist1: "+validList1.length);
                 for (var eachValid1 of validList1) {
                     if (eachValid1 === false)
                         valid = false;
@@ -135,7 +196,12 @@ export default class QueryController {
                 for (var ORfilter of where['OR']) {
                     validList2.push(this.parserEBNF(ORfilter, section));
                 }
-                //      Log.trace("validList2:" + validList2);
+                //    Log.trace("validList2:" + validList2);
+                /*     var ORfilter:any;
+                 for (var key in where['OR'])
+                 {
+                 ORfilter=  where['OR'][key];
+                 validList1.push(this.parserEBNF(ORfilter, section));}  */
 
                 valid = false;
 
@@ -150,84 +216,73 @@ export default class QueryController {
 
 
         if (typeof where['GT'] || typeof where['EQ'] || typeof where['LT'] !== 'undefined') {
-            // var numberRE=new RegExp("^[1-9]*[0-9]+ ('.' [0-9]+ )?$")
+
             if (typeof where['GT'] !== 'undefined') {
 
                 var whereKey1 = Object.keys(where['GT']).toString()
                 var whereValue1 = where['GT'][Object.keys(where['GT'])[0]]
-                //  Log.trace("GT type"+typeof whereValue1)
-                if (this.isvalidKey(whereKey1) === false)
-                    throw Error;
-                //        if(numberRE.test(whereValue1.toString))
+
+                if (this.isvalidKey(whereKey1) === false) {
+                    throw Error
+                }
+                ;
+
                 valid = valid && (section[whereKey1] > whereValue1);
-                //       else
-                //       throw Error;
             }
 
             if (typeof where['EQ'] !== 'undefined') {
                 var whereKey2 = Object.keys(where['EQ']).toString()
                 var whereValue2 = where['EQ'][Object.keys(where['EQ'])[0]]
-                if (this.isvalidKey(whereKey2) === false)
-                    throw Error;
-                //    if(numberRE.test(whereValue2.toString()))
+                if (this.isvalidKey(whereKey2) === false) {
+                    throw Error
+                }
+                ;
                 valid = valid && (((section[whereKey2])) === whereValue2);
-                //       else
-                //      throw Error;
+
             }
 
             if (typeof where['LT'] !== 'undefined') {
 
                 var whereKey3 = Object.keys(where['LT']).toString()
                 var whereValue3 = where['LT'][Object.keys(where['LT'])[0]]
-                if (this.isvalidKey(whereKey3) === false)
-                    throw Error;
-                //      if(numberRE.test(whereValue3.toString()))
+                if (this.isvalidKey(whereKey3) === false) {
+                    throw Error
+                }
+                ;
                 valid = valid && (section[whereKey3] < whereValue3);
-                //    else
-                //         throw Error;
+
             }
         }
 
         if (typeof where['IS'] !== 'undefined') {
-            //   var ISexp = new RegExp('/xxx[\x00-\x7F]+xxx/');
+
             var whereKey4 = Object.keys(where['IS']).toString();
             var whereValue4 = where['IS'][Object.keys(where['IS'])[0]];
-            if (this.isvalidKey(whereKey4) === false)
-                throw Error;
-
+            if (this.isvalidKey(whereKey4) === false) {
+                throw Error
+            }
+            ;
             var sectionWhere = section[whereKey4];
-
+            if (sectionWhere !== "") {
                 if (whereValue4.substring(0, 1) === "*" && whereValue4.substring(whereValue4.length - 1, whereValue4.length) === "*") {
                     whereValue4 = whereValue4.substring(1, whereValue4.length - 1);
-                    /*       if (!ISexp.test(whereValue4))
-                     throw Error;
-                     else    */
                     valid = valid && sectionWhere.includes(whereValue4);
                 }
                 else if (whereValue4.substring(0, 1) === "*") {
                     whereValue4 = whereValue4.substring(1, whereValue4.length);
-                    /*          if (!ISexp.test(whereValue4))
-                     throw Error;
-                     else   */
                     valid = valid && (sectionWhere.substring(sectionWhere.length - whereValue4.length, sectionWhere.length) === whereValue4)
-
                 }
                 else if (whereValue4.substring(whereValue4.length - 1, whereValue4.length) === "*") {
                     whereValue4 = whereValue4.substring(0, whereValue4.length - 1);
-                    /*     if (!ISexp.test(whereValue4))
-                     throw Error;
-                     else      */
                     valid = valid && (sectionWhere.substring(0, whereValue4.length) === whereValue4)
                 }
-
                 else {
-                    /*       if (!ISexp.test(whereValue4))
-                     throw Error;
-                     else   */
                     valid = valid && (sectionWhere === whereValue4);
-
                 }
             }
+            else
+                valid = false;
+        }
 
         if (typeof where['NOT'] !== 'undefined') {
             valid = valid && (!this.parserEBNF(where['NOT'], section));
@@ -284,18 +339,17 @@ export default class QueryController {
         return resultArray;
     }
 
-    public isvalidKey(key: string): boolean {
-        var valid: boolean;
+    public isvalidKey(key: any): any {
+        var isvalidKeyResult: any
         if (key === "courses_dept" || key === "courses_id" || key === "courses_avg" ||
             key === "courses_instructor" || key === "courses_title" || key === "courses_pass" ||
             key === "courses_fail" || key === "courses_audit"
-        )
-            valid = true;
-        else
-            valid = false;
-        return valid;
+        ) {
+            isvalidKeyResult = true;
+        } else {
+            isvalidKeyResult = false;
+        }
+        return isvalidKeyResult;
     }
-
-
 
 }
