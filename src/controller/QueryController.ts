261322bae218233324e6e75172ea74a7be362abd
aliciatang07 <tangxuejie96@163.com>
2016-10-20 15:28:33 -0700

    Add files via upload
    fix  bugs

--- src/controller/QueryController.ts
+++ src/controller/QueryController.ts
@@ -12,7 +12,7 @@ export interface QueryRequest {
     WHERE: {};
     GROUP:string[];
     APPLY:any[];
-    ORDER: string;
+    ORDER: string| any[];
     AS: string;
 }
 
@@ -97,8 +97,8 @@ export default class QueryController {
                 }
 
 
-                //Malibu: APPLY rules should be unique.
-                for (var applyOBJ1 of query.APPLY) {
+               //Malibu: APPLY rules should be unique.
+      /*          for (var applyOBJ1 of query.APPLY) {
                     var duplicateinAPPLY = false
 
                     var applykey1=Object.keys(applyOBJ1)[0]
@@ -115,14 +115,13 @@ export default class QueryController {
                         Log.trace("duplicate keys found in APPLY")
                         return false
                     }
-                }
+                }  */
             }
         }
 
 
         return isValidResult;
     }
-
     public query(query: QueryRequest): QueryResponse {
         Log.trace('QueryController::query( ' + JSON.stringify(query) + ' )');//json string
 
@@ -136,14 +135,20 @@ export default class QueryController {
         var format = query.AS;
 
         var intermediate: any = [];
-
+        var grouplist:any=[];
         if (typeof get === 'string') {
+
             intermediate = this.dealWithWhere(where, get)
         } else {
             intermediate = this.dealWithWhere(where, get[0])
         }
 
-        var values: any = [];
+        if(group!==null && apply!=null)
+        {
+            grouplist=this.dealWithGroup(group,intermediate);
+            intermediate=this.dealWithApply(apply,grouplist);
+        }
+
 
         var finalResultObjArray: any = this.represent(get, intermediate);
 
@@ -155,7 +160,7 @@ export default class QueryController {
         Log.trace("this is FINAL result:  " + JSON.stringify(finalResultObjArray))
         //Log.trace("this is FINAL result:  " + JSON.stringify({render: format, result: finalResultObjArray}))
 
-        return {render: format, result: finalResultObjArray};
+        return {render: format.toLowerCase(), result: finalResultObjArray};
     }
 
 //deal with where
@@ -176,10 +181,12 @@ export default class QueryController {
             /*   for (var key in sections) {
              var section = sections[key]  */
             for (var section of sections) {
-                if (this.parserEBNF(where, section)) {
+                if(where!=null&&Object.keys(where).length>0)
+                { if (this.parserEBNF(where, section)) {
                     //add section to list if it meets WHERE criteria in query
-                    selectedSections.push(section)
-                }
+                    selectedSections.push(section)}}
+                    else
+                { selectedSections.push(section) } ;
             }
         }
         return selectedSections;
@@ -352,28 +359,396 @@ export default class QueryController {
         return resultArray;
 
     }
-    public sortArray(resultArray: any, order: any) {
-        Log.trace("INSIDE sorting!")
-        resultArray.sort(function (a: any, b: any) {
+    public dealWithGroup(group:any,intermediate:any):any{
+        var groups:any=[];
+        /*     if(group.length===1)
+         {while(intermediate.length!=0)
+         {   var sessions:any=[];
+         var groupMap: any = {}
+         var groupvalue:any={};
+         var lastintermediate:any;
+         lastintermediate=intermediate[intermediate.length-1][group[0]];
+         //  Log.trace("lastintermediate+"+ lastintermediate);
+         for (var i=intermediate.length-1;i>=0;i--)
+         {
+         if(intermediate[i][group[0]]===lastintermediate)
+         { Log.trace("lastintermediate+"+ lastintermediate);
+         groupvalue[group[0]]=intermediate[i][group[0]];
+         sessions.push(intermediate[i]);
+         intermediate.splice(i,1);}
+         }
 
-            if (typeof order == "string") {
-                //orderkey is a string
-                var value1 = a[order];
-                //Log.trace("value1  " + value1)
-                var value2 = b[order];
-                if (value1 < value2) {
-                    return -1;
-                }
-                if (value1 > value2) {
-                    return 1;
-                }
-                return 0;
+         groupMap[groupvalue] = sessions;
+         groups.push(groupMap);
+         Log.trace("groupMap"+ JSON.stringify(groupMap));
+         }
+         Log.trace("groups length"+groups.length);
+         }
+         else {  */
+        while(intermediate.length!=0)
+        {   var sessions:any=[];
+            var groupMap: any = {};
+            var lastintermediates:any=[];
+            var groupvalue:any={};
+            for (var a=0;a<group.length;a++)
+            {          var lastintermediate:any;
+                lastintermediate=intermediate[intermediate.length-1][group[a]];
+                lastintermediates.push(lastintermediate);
+                groupvalue[group[a]]=intermediate[intermediate.length-1][group[a]];
+
+
+              //  Log.trace("groupvalue"+JSON.stringify( groupvalue));
+            }
+            sessions.push(groupvalue);
+            for (var i=intermediate.length-1;i>=0;i--)
+            {  if(this.checkGroupCorrect(group,intermediate[i],lastintermediates))
+            //   Log.trace("groupcheck   "+  this.checkGroupCorrect(group,intermediate[i],lastintermediate))
+            { // Log.trace("lastintermediate+"+ lastintermediate);
+                sessions.push(intermediate[i]);
+             //   Log.trace("session.length"+sessions.length);
+                intermediate.splice(i,1); }
+            }
+
+
+            groups.push(sessions);
+
+
+
+        }
+        Log.trace("groups length"+groups.length);
+
+        return groups;
+    }
+
+
+
+    public checkGroupCorrect(group:any,intermediate:any,lastintermediates:any):boolean{
+        var validlist:any=[];
+        var valid:boolean=true;
+        for (var i=0;i< group.length;i++)
+        {
+            if(intermediate[group[i]]===lastintermediates[i])
+            {validlist.push(true);}
+            else
+            { validlist.push(false);}
+        }
+
+        for (var eachValid of validlist) {
+            if (eachValid === false)
+                valid = false;
+        }
+        return valid;
+    }
+/*
+    public dealWithApply(apply:any,grouplist:any):any {
+        var applylist:any=[];
+        var applykeys:any=[];
+        Log.trace("jump into apply")
+        for (var applyobject of apply) {
+            var applynewkey=Object.keys(applyobject)[0];//coursesAvg
+            applykeys.push(applynewkey);
+         //   Log.trace("applynewkey"+applynewkey)
+            var applyvalue=applyobject[Object.keys(applyobject)[0]];
+            var applytoken=Object.keys(applyvalue)[0];//AVG
+            var applystring=applyvalue[Object.keys(applyvalue)[0]];//courses_avg
+            Log.trace("applystring"+applystring);
+            if (applytoken === 'AVG') {
+                Log.trace("jump into AVG122")
+               if(!this.isvalidNumberKey(applystring))
+                  throw Error;
+               else
+                    for (var i = 0; i < grouplist.length; i++) {
+                        var sessions = grouplist[i];
+                        Log.trace("sessions"+JSON.stringify(sessions));
+                        var sum: number=0;
+                        var length:number=0;
+                        for (var j = 1; j<=sessions.length; j++);
+                        {   if( sessions[j][applystring]!=='undefined')
+
+                            {
+                            Log.trace("jump into loop")
+                            sum+=sessions[j][applystring];
+                            length++;  }
+
+
+                        }}
+                        var averageValue: any;
+                        averageValue = parseFloat((sum / length).toFixed(2));
+                        Log.trace("avg"+averageValue);
+                        grouplist[i][0][applynewkey]=averageValue;
+                       Log.trace("group12"+grouplist[i][0]);
+                    }
+            }
+
+            if (applytoken === 'MIN') {
+                if(!this.isvalidNumberKey(applystring))
+                    throw Error;
+                else
+                    for (var i = 0; i < grouplist.length; i++) {
+                        var sessions = grouplist[i];
+                      //  Log.trace("sessions"+JSON.stringify(sessions));
+                        var min: any;
+                        for ( var j = 1; j<=sessions.length; j++);
+                        {
+                            if(sessions[j][applystring]!=='undefined')
+
+                            { if(sessions[j][applystring]<=sessions[j-1][applystring])
+                                min = sessions[j][applystring];
+                            else
+                                min=sessions[j+1][applystring];
+                        }}
+                        grouplist[i][0][applynewkey]=min;
+                   //     Log.trace("group12"+grouplist[i][0]);
+                    }
 
             }
 
-        });
-        return resultArray;
+            if (applytoken === 'MAX') {
+                if(!this.isvalidNumberKey(applystring))
+                    throw Error;
+                else
+                    for (var i = 0; i < grouplist.length; i++) {
+                        var sessions = grouplist[i];
+                     //   Log.trace("sessions"+JSON.stringify(sessions));
+                        var max: any;
+                        for (var j = 1; j<=sessions.length; j++);
+                        {
+                            if( sessions[j][applystring]!=='undefined')
+                            {
+                                if(sessions[j][applystring]>=sessions[j-1][applystring])
+                                    max = sessions[j][applystring];
+                                else
+                                    max=sessions[j+1][applystring];
+                        }}
+                        grouplist[i][0][applynewkey]=max;
+                       // Log.trace("group12"+grouplist[i][0]);
+
+                    }
+            }
+
+
+            if (applytoken === 'COUNT') {
+                for (var i = 0; i < grouplist.length; i++) {
+                    var sessions = grouplist[i];
+                 //   Log.trace("sessions"+JSON.stringify(sessions));
+                    var count=0;
+                    for (var j = 1; j<=sessions.length; j++);
+                    {  if(sessions[j][applystring]!=='undefined')
+                   {
+                        for (var a=0;a<Object.keys(sessions[j]).length;a++)
+                        if(Object.keys(sessions[j])[a]===applystring)
+                        {count+=1;}
+                    }}
+                    grouplist[i][0][applynewkey]=count;
+                   // Log.trace("group12"+grouplist[i][0]);
+                }
+
+            }
+        }
+
+
+        for (var groupobject2 of grouplist){
+          if(this.checkArrayContain(groupobject2,applykeys))
+           applylist.push(groupobject2[0]);
+        }
+        Log.trace("applylist"+applylist);
+        return applylist;
     }
+
+**/
+    public dealWithApply(apply:any,grouplist:any):any {
+        var applylist:any=[];
+
+        Log.trace("jump into apply")
+        for (var applyobject of apply) {
+            var applynewkey=Object.keys(applyobject)[0];//coursesAvg
+          //  Log.trace("applynewkey"+applynewkey)
+            var applyvalue=applyobject[Object.keys(applyobject)[0]];
+            var applytoken=Object.keys(applyvalue)[0];//AVG
+            var applystring=applyvalue[Object.keys(applyvalue)[0]];//courses_avg
+          //  Log.trace("applystring"+applystring);
+            if (applytoken === 'AVG') {
+               // Log.trace("jump into AVG")
+                if(!this.isvalidNumberKey(applystring))
+                    throw Error;
+                else
+                    for (var i = 0; i < grouplist.length; i++) {
+                        var sessions = grouplist[i];
+                        Log.trace("sessions"+JSON.stringify(sessions));
+                        var sum: number=0;
+                        var length:number=0;
+                        for (var j=1;j<sessions.length;j++)
+                        { //  Log.trace("applystring"+applystring);
+                           // Log.trace("123"+sessions[j][applystring])
+                            if( sessions[j][applystring]!=='undefined')
+                             sum+=sessions[j][applystring];
+                            Log.trace("sum111"+sum);
+                                length++;
+                        }
+                       // Log.trace("sum"+sum)
+                       // Log.trace("length  "+length)
+                        var averageValue: any;
+                        averageValue = parseFloat((sum / length).toFixed(2));
+                        Log.trace("avg"+averageValue);
+                        grouplist[i][0][applynewkey]=averageValue;
+                        Log.trace("group12"+grouplist[i][0]);
+                    }
+            }
+
+            if (applytoken === 'MIN') {
+                if(!this.isvalidNumberKey(applystring))
+                    throw Error;
+                else
+                    for (var i = 0; i < grouplist.length; i++) {
+                        var sessions = grouplist[i];
+                        Log.trace("sessions"+JSON.stringify(sessions));
+                        var min: any;
+                        for (var j = 1; j < sessions.length-1; j++)
+                        {
+                            if(sessions[j][applystring]<=sessions[j+1][applystring])
+                                min = sessions[j][applystring];
+                            else
+                                min=sessions[j+1][applystring];
+                        }
+                        grouplist[i][0][applynewkey]=min;
+                        Log.trace("group12"+grouplist[i][0]);
+                    }
+
+            }
+
+            if (applytoken === 'MAX') {
+                if(!this.isvalidNumberKey(applystring))
+                    throw Error;
+                else
+                    for (var i = 0; i < grouplist.length; i++) {
+                        var sessions = grouplist[i];
+                        Log.trace("sessions"+JSON.stringify(sessions));
+                        var max: any;
+                        for (var j = 1; j < sessions.length-1; j++)
+                        {
+                            if(sessions[j][applystring]>=sessions[j+1][applystring])
+                                max = sessions[j][applystring];
+                            else
+                                max=sessions[j+1][applystring];
+                        }
+                        grouplist[i][0][applynewkey]=max;
+                        Log.trace("group12"+grouplist[i][0]);
+
+                    }
+            }
+
+
+            if (applytoken === 'COUNT') {
+                for (var i = 0; i < grouplist.length; i++) {
+                    var sessions = grouplist[i];
+                    Log.trace("sessions"+JSON.stringify(sessions))
+                    var count=0;
+                    for (var j = 1; j < sessions.length-1; j++)
+                    {   for (var a=0;a<sessions[0].length;a++)
+                    { if(Object.keys(sessions[j])[a]===applystring)
+                    {count+=1;}}
+                    }
+                    grouplist[i][0][applynewkey]=count;
+                    Log.trace("group12"+grouplist[i][0]);
+                }
+
+            }
+        }
+        for (var i = 0; i < grouplist.length; i++){
+            applylist.push(grouplist[i][0]);
+        }
+
+      /*   for (var groupobject2 of grouplist){
+         if(this.checkArrayContain(groupobject2,applykeys))
+         applylist.push(groupobject2[0]);
+         }   */
+        //Log.trace("applylist"+applylist);
+        return applylist;
+    }
+
+
+    public checkArrayContain(groupobject2:any,applykeys:any):boolean{
+        Log.trace("jump into check")
+        var valid2:boolean;
+        var validlist:any=[];
+        var groupkeys:any=Object.keys(groupobject2[0])
+        for (var applykey12 in applykeys){
+            valid2=false;
+            for(var groupkey12 in groupkeys)
+            {   if(applykey12===groupkey12)
+            { valid2=true;
+                break;}
+             }
+        validlist.push(valid2)}
+
+        for (var eachValid1 of validlist) {
+            if (eachValid1 === false)
+                valid2 = false;
+        }
+        Log.trace("valid2"+valid2);
+        return valid2;
+
+    }
+
+    public sortArray(resultArray: any, order: any) {
+           // Log.trace("INSIDE sorting!")
+            resultArray.sort(function (a: any, b: any) {
+                if (typeof order == "string") {
+                    //orderkey is a string
+                    var value1 = a[order];
+                    //Log.trace("value1  " + value1)
+                    var value2 = b[order];
+                    if (value1 < value2) {
+                        return -1;
+                    }
+                    if (value1 > value2) {
+                        return 1;
+                    }
+                    return 0;
+
+                }
+
+                else
+                    var orderkey:any=order['keys'];//orderkey is an array
+              //  Log.trace("orderkey"+JSON.stringify(orderkey));
+                var i=0;
+                if(order['dir']==='UP')// lowers come first
+                {  while(i<orderkey.length)
+                {
+                    var value1 = a[orderkey[i]];
+                    var value2 = b[orderkey[i]];
+                //    Log.trace("value1,2up"+value1+ value2)
+                    if (value1 < value2) {
+                        return -1;
+                    }
+                    if (value1 > value2) {
+                        return 1;
+                    }
+                    else
+                        i++; }
+                    return 0;}
+
+                if(order['dir']==='DOWN')
+                {  while(i<orderkey.length)
+                {
+                    var value1 = a[orderkey[i]];
+                    var value2 = b[orderkey[i]];
+                 //   Log.trace("value1,2down"+value1+ value2)
+                    if (value1 < value2) {
+                        return 1;
+                    }
+                    if (value1 > value2) {
+                        return -1;
+                    }
+                    else
+                        i++; }
+                    return 0;}
+
+            });
+           // Log.trace("resultArray"+JSON.stringify( resultArray))
+            return resultArray;
+        }
+
 
     public isvalidKey(key:any):any{
         var isvalidKeyResult:any
@@ -384,6 +759,20 @@ export default class QueryController {
             isvalidKeyResult=true;
         }else{
             isvalidKeyResult=false;
+        }
+        return isvalidKeyResult;
+    }
+
+
+    public isvalidNumberKey(key: any): any {
+        var isvalidKeyResult: any
+        if (key === "courses_avg" ||
+            key === "courses_pass" ||
+            key === "courses_fail" || key === "courses_audit"
+        ) {
+            isvalidKeyResult = true;
+        } else {
+            isvalidKeyResult = false;
         }
         return isvalidKeyResult;
     }
