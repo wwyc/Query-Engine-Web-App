79574ef2e01c56c31af030e8c14e876402ff4bf3
aliciatang07 <tangxuejie96@163.com>
2016-11-23 15:45:01 -0800

    Add files via upload

--- src/rest/public/main.js
+++ src/rest/public/main.js
@@ -1094,15 +1094,6 @@ catch (err) {
         }
     }
 
-    var chart = new FusionCharts ({
-        "type": "column2d",
-        "width": "500",
-        "height": "300",
-        "dataFormat": "json",
-        "dataSource": {
-            chart:{},
-            data: [{value: 500}, {value: 600}, {value: 700}]
-        }
-    }).render("chartContainer");
+
 });
 
