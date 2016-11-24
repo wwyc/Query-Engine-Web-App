fbe05924028074cee1abd87112b0bc762c045c8c
wchan <Because1215>
2016-11-23 21:39:28 -0800

    Winnie Committed 112316a  (updated chart functionality)

--- src/rest/public/main.js
+++ src/rest/public/main.js
@@ -993,39 +993,8 @@ catch (err) {
 
 
 
-/*
-    $("#chart-container").insertFusionCharts({
-        type: "column2d",
-        width: "400",
-        height: "350",
-        dataFormat: "json",
-        dataSource: {
-            chart: {
-                caption: "Harry's SuperMart",
-                subCaption: "Top 5 stores in last month by revenue",
-                numberPrefix: "$",
-                theme: "ocean"
-            },
-            data: [{
-                label: "Bakersfield Central",
-                value: "880000"
-            }, {
-                label: "Garden Groove harbour",
-                value: "730000"
-            }, {
-                label: "Los Angeles Topanga",
-                value: "590000"
-            }, {
-                label: "Compton-Rancho Dom",
-                value: "520000"
-            }, {
-                label: "Daly City Serramonte",
-                value: "330000"
-            }]
-        }
-    });
 
-*/
+
     function generateTable(data) {
         var columns = [];
         Object.keys(data[0]).forEach(function (title) {
@@ -1095,5 +1064,38 @@ catch (err) {
     }
 
 
+});
+
+$(function() {
+    $("#chart-container").insertFusionCharts({
+        type: "column2d",
+        width: "800",
+        height: "700",
+        dataFormat: "json",
+        dataSource: {
+            chart: {
+                caption: "Enter Title of Chart",
+                subCaption: "Enter SubCaption of Chart",
+                numberPrefix: "$",
+                theme: "ocean"
+            },
+            data: [{
+                label: "Bakersfield Central",
+                value: "880000"
+            }, {
+                label: "Garden Groove harbour",
+                value: "730000"
+            }, {
+                label: "Los Angeles Topanga",
+                value: "590000"
+            }, {
+                label: "Compton-Rancho Dom",
+                value: "520000"
+            }, {
+                label: "Daly City Serramonte",
+                value: "330000"
+            }]
+        }
+    });
 });
 
